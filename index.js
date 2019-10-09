6079592d097dbde8c7791417fe2091d9d7b44393
Prappo Prince <prappo.prince@gmail.com>
2019-10-10 01:35:33 +0600

    desigining structure

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var Twitter = require('twitter');
 var config = require('./config');
+var menu = require('./lib/menu');
 var client = new Twitter({
     consumer_key: config.consumer_key,
     consumer_secret: config.consumer_secret,
@@ -8,6 +9,7 @@ var client = new Twitter({
 });
 
 
+menu.showMenu();
 
 // var api_key = config.api_key;
 // var domain = 'mail.trino-lab.com';
@@ -67,33 +69,6 @@ var client = new Twitter({
 //     process.exit() ;
 // } ) ;
 
-var Menu = require('terminal-menu');
-var menu = Menu({ width: 29, x: 4, y: 2 });
-menu.reset();
-menu.write('Twitter Tool\n');
-menu.write('-------------------------\n');
-
-menu.add('Post Tweet');
-menu.add('BUSINESS INTELLIGENCE');
-menu.add('ACCOUNTS PAYABLE');
-menu.add('LEDGER BOOKINGS');
-menu.add('INDICATOR CHART METRICS');
-menu.add('BACKUP DATA TO FLOPPY DISK');
-menu.add('RESTORE FROM FLOPPY DISK');
-menu.add('EXIT');
-
-menu.on('select', function (label) {
-    // menu.close();
-    console.log('SELECTED: ' + label);
-
-});
-process.stdin.pipe(menu.createStream()).pipe(process.stdout);
-
-process.stdin.setRawMode(true);
-menu.on('close', function () {
-    process.stdin.setRawMode(false);
-    process.stdin.end();
-});
 
 
 
